5a4c3a528ad32fb6f5cc5bb8184a6e47fba9c8ba
clement <clementchpoon@gmail.com>
2023-02-16 23:25:49 +0000

    rm debug

--- build_em.sh
+++ build_em.sh
@@ -1,4 +1,4 @@
-DEMANGLE_SUPPORT=1 EMCC_DEBUG=1 emcc -g main.cpp notes.cpp -s ASSERTIONS=2 -s WASM=1 -s USE_SDL=2 -s USE_SDL_MIXER=2 -s USE_SDL_TTF=2 -o tetris.html -DEMCXX  --preload-file "./font.ttf"
+emcc main.cpp notes.cpp -s WASM=1 -s USE_SDL=2 -s USE_SDL_MIXER=2 -s USE_SDL_TTF=2 -o tetris.html -DEMCXX -O3  --preload-file "./font.ttf"
 
 
 
